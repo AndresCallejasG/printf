afc9eff972a140795ef25e1723b6399c62f13958
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-16 22:55:32 +0000

    Principal function, print with format

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ * _printf - Print with format
+ *
+ * @format: format
+ * Return: number of characters printed
+ * -1 if doesn't work.
+ */
+int _printf(const char *format, ...)
+{
+	va_list valist;
+	int i, j, cont, match = 0;
+
+	p_func opt[] = {{"c", p_char}, {"s", p_str}, {"i", p_int},
+	{"d", p_float}, {0, 0}};
+	if (format == 0)
+		return (-1);
+	va_start(valist, format);
+	for (i = 0; format[i] != 0; i++)
+	{
+		if (format[i] != '%')
+			_putchar(format[i]), cont++;
+		else if (format[i] == '%' && format[i + 1] != '%')
+		{
+			for (j = 0; opt[j].fmt != 0; j++)
+			{
+				if (format[i + 1] == opt[j].fmt[0])
+				{
+					cont += opt[j].f(valist);
+					i++;
+					match = 1;
+				}
+			}
+			if (match != 1)
+				_putchar('%'), cont++;
+			match = 0;
+		}
+		else
+		{
+			_putchar('%'), cont++, i++;
+		}
+	}
+	va_end(valist);
+	return (cont);
+}
