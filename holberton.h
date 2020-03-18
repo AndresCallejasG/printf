ea3aa87d811ae8c367a34dff4aedd7b1dd47270d
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-18 00:49:28 +0000

    include %b %o %x %X %u

--- holberton.h
+++ holberton.h
@@ -20,11 +20,18 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 int _strlen(char *s);
 int print_int(int n);
+int print_un_int(unsigned int n);
+int base_conv(unsigned int num, int base, int mayus);
 
 int p_char(va_list valist);
 int p_int(va_list valist);
 int p_dint(va_list valist);
 int p_str(va_list valist);
 int p_rev(va_list valist);
+int p_bin(va_list valist);
+int p_oct(va_list valist);
+int p_hex(va_list valist);
+int p_hex_case(va_list valist);
+int p_un_int(va_list valist);
 
 #endif
