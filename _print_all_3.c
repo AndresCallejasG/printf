2bef2fa44de5c29e9391b6bfeb8970320aba95da
Felipe Layos <feliperodriguezlayos@gmail.com>
2020-03-18 02:20:41 +0000

    function rot13

---
+++ _print_all_3.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * p_rot13 - encodes a string using rot13.
+ *
+ * @valist: string
+ *
+ * Return: number of bytes printed.
+ */
+int p_rot13(va_list valist)
+{
+	int i, aux;
+	char *s;
+
+	s = va_arg(valist, char *);
+
+	i = 0;
+	aux = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		aux = 13 + s[i];
+
+		if ((aux > 'Z' && s[i] <= 'Z') || aux > 'z')
+		{
+			aux = aux % (90 + (32 * (97 / s[i])));
+			aux = aux + (65 + (32 * (97 / s[i]))) - 1;
+		}
+		_putchar(aux);
+	}
+	return (i);
+}
