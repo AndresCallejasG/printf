7f1b9ff822f2f1d73fd1cc3929ff5a8d316e8cc0
Andres Callejas G <andres.callejasg@gmail.com>
2020-03-18 00:47:56 +0000

    functions for %b %o %x %X %u

--- _print_all_2.c
+++ _print_all_2.c
@@ -31,3 +31,72 @@ int p_rev(va_list valist)
 	}
 	return (count);
 }
+/**
+* p_bin - prints a unsigned int as binary
+* @valist: string
+*
+* Return: void
+*/
+int p_bin(va_list valist)
+{
+	int size = 0;
+
+	int num = va_arg(valist, int);
+
+	size = base_conv(num, 2, 0);
+
+	return (size);
+}
+/**
+* p_oct - prints a unsigned int as octal
+* @valist: string
+*
+* Return: void
+*/
+int p_oct(va_list valist)
+{
+	int size = 0;
+
+	int num = va_arg(valist, int);
+
+	size = base_conv(num, 8, 0);
+
+	return (size);
+}
+/**
+* p_hex - prints a unsigned int as hexadecimal
+* with lowercase letters
+* @valist: string
+*
+* Return: void
+*/
+int p_hex(va_list valist)
+{
+	int size = 0;
+
+	int num = va_arg(valist, int);
+
+	size = base_conv(num, 16, 0);
+
+	return (size);
+}
+/**
+* p_hex_case - prints a unsigned int as hexadecimal
+* with uppercase letters
+* @valist: string
+*
+* Return: void
+*/
+int p_hex_case(va_list valist)
+{
+	int size = 0;
+
+	int num = va_arg(valist, int);
+
+	size = base_conv(num, 16, 1);
+
+	return (size);
+}
+
+
+
